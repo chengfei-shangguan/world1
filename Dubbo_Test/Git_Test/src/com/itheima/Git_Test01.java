9f60ed330fbfa0dc5343e1c4ffb3a907a0a4f599
HUGO <scf180015@163.com>
2019-09-22 20:00:57 +0800

    提交GIt_Test到本地仓库

---
+++ Dubbo_Test/Git_Test/src/com/itheima/Git_Test01.java
@@ -0,0 +1,14 @@
+package com.itheima;
+
+public class Git_Test01 {
+    public static void main(String[] args) {
+        System.out.println("hello----------Git");
+        System.out.println("hello----------Git");
+        System.out.println("hello----------Git");
+        System.out.println("hello----------Git");
+        System.out.println("hello----------Git");
+        System.out.println("hello----------Git");
+
+
+    }
+}
